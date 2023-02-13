c5ebe1ec1f6ad61bdad056ac9b266bbf15d44d8c
Ahmad Malik <arma7x@live.com>
2023-02-14 03:57:25 +0800

    fetch pricecatcher.zip from https://github.com/arma7x/opendosm-parquet-to-sqlite && build:BREAK

--- lib/modules/PriceCatcher/PriceCatcherScreen.dart
+++ lib/modules/PriceCatcher/PriceCatcherScreen.dart
@@ -41,6 +41,7 @@ class _PriceCatcherScreenState extends State<PriceCatcherScreen> {
   }
 
   _fetchData() async {
+    await Api.GetDatabaseWeb();
     try {
       _loadingDialog(true);
 
--- lib/modules/PriceCatcher/api.dart
+++ lib/modules/PriceCatcher/api.dart
@@ -6,7 +6,72 @@ import 'package:html/dom.dart' as DOM;
 import 'package:csv/csv.dart';
 import '../ApiCommon.dart';
 
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:archive/archive_io.dart';
+import 'package:idb_shim/idb.dart';
+import 'package:idb_shim/idb_browser.dart';
+import 'dart:typed_data';
+import 'package:sqlite3/common.dart';
+import 'package:sqlite3/wasm.dart';
+
 class Api with ApiHelper {
+
+  static GetDatabaseWeb() async {
+    try {
+      IdbFactory idbFactory = getIdbFactory()!;
+      final storeName = 'opendosm';
+      final db = await idbFactory.open('price_catcher', version: 1,
+          onUpgradeNeeded: (VersionChangeEvent event) {
+          final db = event.database;
+          db.createObjectStore(storeName, autoIncrement: true);
+      });
+
+      var txn = db.transaction(storeName, "readonly");
+      var store = txn.objectStore(storeName);
+      var byteArray = await store.getObject("zip");
+      await txn.completed;
+
+      if (byteArray == null) {
+        final response = await http.get(Uri.parse("https://raw.githubusercontent.com/arma7x/opendosm-parquet-to-sqlite/master/pricecatcher.zip"));
+        if (response.statusCode == 200) {
+          // String dir = (await getTemporaryDirectory()).path;
+          txn = db.transaction(storeName, "readwrite");
+          store = txn.objectStore(storeName);
+          await store.put(response.bodyBytes, "zip");
+          await store.put(response.contentLength!, "contentLength");
+          await txn.completed;
+          byteArray = response.bodyBytes;
+        } else {
+          throw('Unknown error');
+        }
+      }
+      Uint8List dbUint8List = Uint8List(0);
+      final archive = ZipDecoder().decodeBytes(byteArray as Uint8List);
+      for (final file in archive) {
+        if (file.isFile) {
+          dbUint8List = file.content as Uint8List;
+          break;
+        }
+      }
+      final response = await http.get(Uri.parse('sqlite3.wasm'));
+      final fs = await IndexedDbFileSystem.open(dbName: 'opendosm_pricecatcher_database');
+      if (!fs.exists("/pricecatcher.db")) {
+        fs..createFile("/pricecatcher.db")..write("/pricecatcher.db", dbUint8List as Uint8List, 0);
+      }
+      final sqlite = await WasmSqlite3.load(response.bodyBytes, SqliteEnvironment(fileSystem: fs));
+      final dbSQL = await sqlite.open('/pricecatcher.db');
+      print(dbSQL.select('''
+        SELECT *
+        FROM items
+        LIMIT 2
+        '''
+      ));
+    } on Exception catch (e) {
+      throw(e);
+      // rethrow;
+    }
+  }
 
   static Future<List<Map<String, String>>> GetPriceCatcherList() async {
     try {
