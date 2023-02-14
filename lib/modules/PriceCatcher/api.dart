9645cb0a9e96f730b663a84429cc697420af0b55
Ahmad Malik <arma7x@live.com>
2023-02-14 22:53:21 +0800

    remove scrapping API

--- lib/modules/PriceCatcher/api.dart
+++ lib/modules/PriceCatcher/api.dart
@@ -1,10 +1,6 @@
 import 'dart:core';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' show parse;
-import 'package:html/dom.dart' as DOM;
-import 'package:csv/csv.dart';
-import '../ApiCommon.dart';
 
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
@@ -15,7 +11,7 @@ import 'dart:typed_data';
 import 'package:sqlite3/common.dart';
 import 'package:sqlite3/wasm.dart';
 
-class Api with ApiHelper {
+class Api {
 
   static Future<CommonDatabase> GetDatabaseWeb() async {
     int latestContentLength = 0;
@@ -71,35 +67,6 @@ class Api with ApiHelper {
       final sqlite = await WasmSqlite3.load(response.bodyBytes, SqliteEnvironment(fileSystem: fs));
       final instance = await sqlite.open('/pricecatcher.db');
       return Future<CommonDatabase>.value(instance);
-    } on Exception catch (e) {
-      rethrow;
-    }
-  }
-
-  static Future<List<Map<String, String>>> GetPriceCatcherList() async {
-    try {
-      final response = await http.get(Uri.parse("https://open.dosm.gov.my/data-catalogue"));
-      if (response.statusCode == 200) {
-        final DOM.Document document = parse(response.body);
-        List<DOM.Element> sections = document.getElementsByTagName("section");
-        List<DOM.Element> ul = [];
-        for (var i = 0; i < sections.length; i++) {
-          if (sections[i].children.length > 0 && sections[i].children[0].text.trim() == "Economy: PriceCatcher") {
-            ul = [...sections[i].children[1].children];
-            break;
-          }
-        }
-        List<Map<String, String>> priceCatcher = [];
-        ul = ul.reversed.toList();
-        ul.forEach((li) {
-          if (li.children[0].text.indexOf("PriceCatcher") > -1) {
-            priceCatcher.add({ "name": li.children[0].text, "href": "https://open.dosm.gov.my${li.children[0].attributes['href']!}" });
-          }
-        });
-        return Future<List<Map<String, String>>>.value(priceCatcher);
-      } else {
-        throw('Unknown error');
-      }
     } on Exception catch (e) {
       rethrow;
     }
